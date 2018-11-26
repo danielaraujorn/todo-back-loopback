af187e67f88fe5ad7f7eeb0215b6dc996ded6630
Daniel Araújo <daniel.araujorn01@gmail.com>
2018-11-26 12:55:18 -0300

    checking

--- common/models/checkbox.js
+++ common/models/checkbox.js
@@ -1,3 +1,20 @@
 "use strict";
 
-module.exports = function(Checkbox) {};
+module.exports = function(Checkbox) {
+  Checkbox.remoteMethod("check", {
+    accepts: [
+      { arg: "id", type: "string", required: true },
+      { arg: "checked", type: "boolean", required: true }
+    ],
+    http: { path: "/check/", verb: "put" },
+    returns: { arg: "checkbox", type: "object" }
+  });
+  Checkbox.check = (id, checked, callback) => {
+    console.log(id, checked);
+    Checkbox.findById(id, (err, item) =>
+      item.updateAttribute("checked", checked, (err, updated) =>
+        callback(null, updated)
+      )
+    );
+  };
+};
